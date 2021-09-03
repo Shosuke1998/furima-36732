0aa369a312130adfa8cb33ee2655f96995688302
Shosuke1998 <shosuke123@outlook.jp>
2021-09-03 14:25:50 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :content, :category_id, :condition_id, :shipping_charge_id, :region_id, :days_to_ship_id, :price, :user_id)
+  end
 end
